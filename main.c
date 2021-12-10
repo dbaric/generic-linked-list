1df55b216ecdce2573750ac5ef41c6d407763d93
Dražen Barić <drazenbaric@hotmail.com>
2021-12-10 01:17:54 +0100

    Draft: Brainstorm

--- main.c
+++ main.c
@@ -1,7 +1,7 @@
 #include<stdio.h>
 #include<stdlib.h>
 
-/* */
+/* Zadano: */
 
 struct _Node;
 typedef struct _Node* Position;
@@ -9,91 +9,53 @@ typedef struct _Node {
     Position next;
 } Node;
 
-/* */
-
-struct _IntegerNode;
-typedef struct _IntegerNode* IntegerPosition;
-typedef struct _IntegerNode {
-    int data;
-    struct _Node node;
-} IntegerNode;
-
-IntegerPosition create_integer_node(int data);
-IntegerPosition create_integer_node(int data) {
-    IntegerPosition new_integer_node = NULL;
-    new_integer_node = (IntegerPosition)malloc(sizeof(IntegerNode));
-
-    if(!new_integer_node) {
-        return NULL;
-    }
-
-    new_integer_node->node.next = NULL;
-    new_integer_node->data = data;
-
-    return new_integer_node;
-}
-
-int add_after_integer_node(IntegerPosition target_node, IntegerPosition new_node);
-int add_after_integer_node(IntegerPosition target_node, IntegerPosition new_node) {
-    Position temp = target_node->node.next; // spremi trenutni target next
-
-    target_node->node.next = new_node; // target ima novi new
-    new_node->node.next = temp; // a new ima (stari) target next
-
-    return 0;
-}
-
-int print_all_integer_nodes(IntegerPosition head);
-int print_all_integer_nodes(IntegerPosition head) {
-    IntegerPosition temp = head;
-
-    printf("Print all node data: \n");
-
-    while(temp) {
-        printf(" | %i | ", temp->data);
-        temp = temp->node.next;
-    }
-
-    printf("\n");
-
-    return 0;
-}
-
-// implementiraj mene
-int delete_node() {
-    return 0;
-}
+/* * * * */
 
 int main() {
+    // dummy data
+    int number = 1950;
+    char letter = 'h';
+    // struct Node - predefined
+
+    // pointers to types of data
+    int* number_pointer = &number;
+    char* letter_pointer = &letter;
+    // struct Node* (Position)- predefined
+
+    // allocate and print memory
+    // pazi: ovisi je li 32bit ili 64bit CPU
+    int size_of_number_pointer = sizeof(number_pointer); // (4)
+    int size_of_letter_pointer = sizeof(letter_pointer); // (4)
+    int size_of_node = sizeof(Node); // (4)
+    printf("sizeof(number_pointer): %i \n address: %p \n", sizeof(size_of_number_pointer), number_pointer);
+    printf("sizeof(letter_pointer): %i \n address: %p \n", sizeof(size_of_letter_pointer), letter_pointer);
+    printf("sizeof(Node): %i \n address: %p \n", sizeof(size_of_letter_pointer), letter_pointer);
+    /*
+     dovoljno dobra pretpostavka:
+    */
+    int size_of_data_pointer = size_of_number_pointer;
+
+    // ideja: rezervirat (i počistit) više byte-ova nego što treba
+    // na tu poziciju stavljat pointer naprijed
+    // je li ovo positive overflow? istraži!!
+    Position node_1 = calloc(
+        sizeof(Node) + sizeof(size_of_data_pointer),
+        size_of_data_pointer
+    );
+    /*
+    calloc:
+    num - Number of elements to allocate.
+    size - Size of each element.
+    */
 
     /*
-    * extend za brojeve
+        ideja: imam 4 byte-ova na raspolaganju, zadnjih 4 (ukupno 8)
+
+        kako "spojiti" pisanje u zadnja 4 (naš pointer) da bude koristiv?
+        - imamo "početnu" addresu
+        - znam točan broj byte-ova
+        - mogu iterirat i postavljat kroz pointer adrese vrijednost u memoriju
     */
-    IntegerPosition integer_node_head = create_integer_node(0);
-
-    if(!integer_node_head) {
-        return -1;
-    }
-
-    for(int i = 1; i < 10; i++) {
-        IntegerPosition new_integer_node = create_integer_node(i);
-
-        if(!new_integer_node) {
-            return -1;
-        }
-
-        new_integer_node->data = i;
-
-        add_after_integer_node(integer_node_head, new_integer_node);
-    }
-
-    // znam da je ispis 0 9 8 ..., zato što za head šaljem "0" odnosno na njega kačim nove elemente
-    print_all_integer_nodes(integer_node_head);
-
-
-    // puni charovima
-
-    // puno custom structom
 
     return 0;
 }
